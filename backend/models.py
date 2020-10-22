2f8133a286c1bc467464fb834a02c64fd7d3ad60
scwatts <s.watts2@student.unimelb.edu.au>
2020-10-22 11:27:18 +1100

    Remove results model from backend

--- backend/models.py
+++ backend/models.py
@@ -10,8 +10,3 @@ class Job(models.Model):
     input_file = models.CharField(max_length=300)
     status = models.CharField(max_length=20, default='initialising')
     created_at = models.DateTimeField(auto_now_add=True)
-
-
-class Result(models.Model):
-    name = models.CharField(max_length=300)
-    created_at = models.DateTimeField(auto_now_add=True)
