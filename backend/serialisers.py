d97bc50512f9fdd75a9f52680429cc3f9db99e98
Stephen Watts <hello@stephen.ac>
2024-11-03 12:24:12 +1100

    Require exactly one input file

--- backend/serialisers.py
+++ backend/serialisers.py
@@ -1,3 +1,5 @@
+import re
+
 import django.contrib.auth.models
 import rest_framework.serializers
 
@@ -12,7 +14,14 @@ class JobSerialiser(rest_framework.serializers.ModelSerializer):
         fields = '__all__'
 
     def validate(self, data):
+
         # Ensure that we have file data before attempting to write file data to disk
         if self.context.get('file_validation', True) and not self.context.get('file', False):
             raise rest_framework.serializers.ValidationError({'file': 'A file is required'})
+
+        # Require exactly one file
+        file_count = len(self.context.get('file'))
+        if file_count != 1:
+            raise rest_framework.serializers.ValidationError({'file': f'Expected one file but got {file_count}'})
+
         return data
